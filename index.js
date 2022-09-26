f88f5a288053363d188d83e5605853b867f7e21b
Collins Kiprono Rotich <kolinsr@gmail.com>
2022-09-26 07:46:24 +0300

    Verified the day of week function

--- index.js
+++ index.js
@@ -6,9 +6,20 @@
             alert("Enter correct birthday!");
         }else{
                 alert("Your birthday is " + birthday);
-
             }
+        
+        var gender = document.querySelector('input[name="gender"]:checked').value;
+
+        function getDayOfWeek(date) {
+            const dayOfWeek = new Date(date).getDay();    
+            return isNaN(dayOfWeek) ? null : 
+              ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
+          }
+          day = getDayOfWeek(birthday);
+
+          alert("Your birthdate is " + day);
         }
+
         
     
 
