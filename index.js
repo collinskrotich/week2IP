0903b58ce3c5834cf029ed02b306a4b45076875c
Collins Kiprono Rotich <kolinsr@gmail.com>
2022-09-26 06:40:38 +0300

    Added Javascript function for picking date

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-var name = window.prompt("Enter your name: ")
-alert("Your name is " + name);
+
+    document.getElementById("submit").onclick = function(){
+
+        var birthday = document.getElementById("bdate").value;
+        if (!birthday){
+            alert("Enter correct birthday!");
+        }else{
+                alert("Your birthday is " + birthday);
+
+            }
+        }
+        
+    
+
+
