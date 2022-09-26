b611af7162afd849a518b89cbeabb8c6072ae1b0
Collins Kiprono Rotich <kolinsr@gmail.com>
2022-09-26 20:28:23 +0300

    Modified CSS for the index file

--- index.js
+++ index.js
@@ -3,19 +3,22 @@
         event.preventDefault();
 
         var birthday = document.getElementById("bdate").value;
+        var gender = document.querySelector('input[name="gender"]:checked').value;
+        
         if (!birthday){
             alert("Enter correct birthday!");
         }else if(!gender){
                 alert("Please enter your gender");
             }else {
         
-        var gender = document.querySelector('input[name="gender"]:checked').value;
+
 
         function getDayOfWeek(date) {
             const dayOfWeek = new Date(date).getDay();    
             return isNaN(dayOfWeek) ? null : 
               ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
           }
+
           day = getDayOfWeek(birthday);
 
           if(gender == "Male" && day == "Sunday"){
@@ -24,77 +27,73 @@
           }
             else if(gender == "Male" && day == "Monday")
           {
-            alert("Your akan name is Kwadwo");
-            var akan = "Kwasi";
+            var akan = "Kwadwo";
             document.getElementById("akan_name").innerHTML = akan;
           }
             else if(gender == "Male" && day == "Tuesday")
-         {
-            alert("Your akan name is Kwabena");
-            var akan = "Kwasi";
+          {
+            var akan = "Kwabena";
             document.getElementById("akan_name").innerHTML = akan;
-         }
+          }
             else if(gender == "Male" && day == "Wednesday")
-         {
-            alert("Your akan name is Kwaku");
-            var akan = "Kwasi";
+          {
+            var akan = "Kwaku";
             document.getElementById("akan_name").innerHTML = akan;
-         }
+          }
             else if(gender == "Male" && day == "Thursday")
-         {
-            alert("Your akan name is Yaw");
-            var akan = "Kwasi";
+          {
+            var akan = "Yaw";
             document.getElementById("akan_name").innerHTML = akan;
-         }
+          }
             else if(gender == "Male" && day == "Friday")
-         {
+          {
             var akan = "Kofi";
             document.getElementById("akan_name").innerHTML = akan;
-         }
+          }
             else if(gender == "Male" && day == "Saturday")
-         {
+          {
             var akan = "Kwame";
             document.getElementById("akan_name").innerHTML = akan;
 
-         }
+          }
             else if(gender == "Female" && day == "Sunday")
-         {
+          {
             var akan = "Akosua";
             document.getElementById("akan_name").innerHTML = akan;
-         }
+          }
             else if(gender == "Female" && day == "Monday")
-         {
+          {
             var akan = "Adwoa";
             document.getElementById("akan_name").innerHTML = akan;
-         }
+          }
             else if(gender == "Female" && day == "Tuesday")
-         {
+          {
             var akan = "Abenaa";
             document.getElementById("akan_name").innerHTML = akan;
-         }
+          }
             else if(gender == "Female" && day == "Wednesday")
-         {
+          {
             var akan = "Akua";
             document.getElementById("akan_name").innerHTML = akan;
-         }
+          }
             else if(gender == "Female" && day == "Thursday")
-         {
+          {
             var akan = "Yaa";
             document.getElementById("akan_name").innerHTML = akan;
-         }
+          }
             else if(gender == "Female" && day == "Friday")
-         {
+          {
             var akan = "Afua";
             document.getElementById("akan_name").innerHTML = akan;
-         }
+          }
             else if(gender == "Female" && day == "Saturday")
-         {
+          {
             var akan = "Ama";
             document.getElementById("akan_name").innerHTML = akan;
-         }
+          }
             else
-         {
+          {
             alert("Something is wrong");
+          }
          }
-        }
         }
